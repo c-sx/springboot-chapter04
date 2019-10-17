4dd0f13d74f02b92b61357c260de8163da8313fb
Cecil <csx17@qq.com>
2019-10-17 15:43:42 +0800

    Initialize the new project about Spring Security

---
+++ src/main/java/com/mpocket/springbootchapter04/SpringbootChapter04Application.java
@@ -0,0 +1,13 @@
+package com.mpocket.springbootchapter04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootChapter04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootChapter04Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mpocket/springbootchapter04/SpringbootChapter04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpocket.springbootchapter04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootChapter04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
